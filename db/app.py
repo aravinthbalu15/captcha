809356b5405569f797549082d9ac31e81dc720fa
batuhannoz <b.ozdmr23@gmail.com>
2024-03-07 00:36:39 +0300

    Create db with random 1000 captcha images

---
+++ db/app.py
@@ -0,0 +1,67 @@
+import random
+import string
+import time
+from concurrent.futures._base import LOGGER
+from datetime import datetime
+
+import psycopg2
+from captcha.image import ImageCaptcha
+
+# PostgreSQL bağlantı bilgileri
+db_params = {
+    'host': 'postgres',
+    'dbname': 'postgres',
+    'user': 'myuser',
+    'password': 'mypassword',
+    'port': '5432'
+}
+
+# PostgreSQL bağlantısını oluştur
+
+while True:
+    try: 
+        conn = psycopg2.connect(**db_params)
+        if conn:
+            break
+    except Exception as e:
+        LOGGER.warning(f"++++ Retrying connection to the database because of the issue {str(e)}++++")
+    time.sleep(3)
+        
+cursor = conn.cursor()
+
+# Tabloyu oluştur (Eğer daha önce oluşturulmadıysa)
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS captchas (
+        id SERIAL PRIMARY KEY,
+        code CHAR(6),
+        captcha_image BYTEA,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+""")
+
+conn.commit()
+
+
+# Captcha oluştur
+def generate_captcha():
+    random_string = ''.join(random.choices(string.ascii_uppercase, k=6))
+    captcha: ImageCaptcha = ImageCaptcha(width=400,
+                                         height=220,
+                                         fonts=['/times.ttf',],
+                                         font_sizes=(70, 90))
+    image_data = captcha.generate(random_string)
+    image_bytes = image_data.getvalue()
+    return random_string, image_bytes
+
+
+# Captcha'yı oluştur ve PostgreSQL veritabanına kaydet
+
+for _ in range(1000):
+    code, captcha_image = generate_captcha()
+    cursor.execute("INSERT INTO captchas (code, captcha_image, created_at) VALUES (%s, %s, %s)",
+                   (code, psycopg2.Binary(captcha_image), datetime.now(),))
+    
+conn.commit()
+# Bağlantıyı kapat
+cursor.close()
+conn.close()
