20eabfae0ca9dd1d64dd2794583b43c33afb4ee9
batuhannoz <b.ozdmr23@gmail.com>
2024-03-13 11:16:14 +0300

    Initialize spring boot project

---
+++ captcha/src/test/java/com/duzce/captcha/CaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duzce.captcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
