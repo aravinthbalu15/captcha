59c43846f79495c879df7002b9c3e080831b78c7
batuhannoz <b.ozdmr23@gmail.com>
2024-04-29 20:58:10 +0300

    Add delete, insert and search Captcha

--- captcha/src/main/java/com/duzce/captcha/dao/CaptchaRepository.java
+++ captcha/src/main/java/com/duzce/captcha/dao/CaptchaRepository.java
@@ -1,18 +1,14 @@
 package com.duzce.captcha.dao;
 
 import com.duzce.captcha.model.Captcha;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Expression;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.criteria.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -25,14 +21,82 @@ public class CaptchaRepository {
         return sessionFactory.getCurrentSession();
     }
 
-    public Captcha findRandomCaptcha() {
-        Session session = sessionFactory.openSession();
+    public Captcha getRandomCaptcha() {
+        Captcha captcha = new Captcha();
         try {
             String query = "SELECT c FROM Captcha c ORDER BY FUNCTION('RANDOM') LIMIT 1";
-            Captcha result = session.createQuery(query, Captcha.class).getSingleResult();
-            return result;
-        } finally {
-            session.close();
+            captcha = getSession().createQuery(query, Captcha.class).getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return captcha;
+    }
+
+    public List<Captcha> getCaptchas(int setFirstResult, int setMaxResults) {
+        List<Captcha> captchas = null;
+        try {
+            CriteriaBuilder criteriaBuilder = getSession().getCriteriaBuilder();
+            CriteriaQuery<Captcha> criteriaQuery = criteriaBuilder.createQuery(Captcha.class);
+
+            Root<Captcha> root = criteriaQuery.from(Captcha.class);
+
+            criteriaQuery
+                    .select(root)
+                    .orderBy(criteriaBuilder.asc(root.get("id")));
+
+            Query<Captcha> query = getSession().createQuery(criteriaQuery);
+            query.setFirstResult(setFirstResult);
+            query.setMaxResults(setMaxResults);
+            captchas = query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return captchas;
+    }
+
+    public Captcha getCaptchaById(int id) {
+        try {
+            CriteriaBuilder criteriaBuilder = getSession().getCriteriaBuilder();
+            CriteriaQuery<Captcha> criteriaQuery = criteriaBuilder.createQuery(Captcha.class);
+            Root<Captcha> root = criteriaQuery.from(Captcha.class);
+            criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("id"), id));
+            return getSession().createQuery(criteriaQuery).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    public void deleteCaptcha(int id) {
+        try {
+            CriteriaBuilder criteriaBuilder = getSession().getCriteriaBuilder();
+            CriteriaDelete<Captcha> criteriaDelete = criteriaBuilder.createCriteriaDelete(Captcha.class);
+            Root<Captcha> root = criteriaDelete.from(Captcha.class);
+            criteriaDelete.where(criteriaBuilder.equal(root.get("id"), id));
+            getSession().createQuery(criteriaDelete).executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getRowCount() {
+        int rowCount = -1;
+        try {
+            CriteriaBuilder criteriaBuilder = getSession().getCriteriaBuilder();
+            CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
+            Root<Captcha> root = countQuery.from(Captcha.class);
+            countQuery.select(criteriaBuilder.count(root));
+            rowCount =  getSession().createQuery(countQuery).getSingleResult().intValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rowCount;
+    }
+
+    public void insertCaptcha(Captcha captcha) {
+        try {
+            getSession().save(captcha);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
